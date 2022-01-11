95f5b917a84640aa008959d6411b7110c38e7a1c
shabazzm <shabazzm@oregonstate.edu>
2022-01-10 17:30:51 -0700

    Added functinality to flip a coin. Clicking button "flip coin" produces either heads or tails in text view

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,6 +12,23 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
+
+        val rollButton2: Button = findViewById(R.id.button2)
+        rollButton2.setOnClickListener { flipCoin() }
+    }
+
+    private fun flipCoin() {
+        val coin = Coin()
+        val coinFlip = coin.flip()
+        val headsText = "Heads"
+        val tailsText = "Tails"
+        val resultTextView2: TextView = findViewById(R.id.textView2)
+        if (coinFlip == 1) {
+            resultTextView2.text = headsText
+        } else {
+            resultTextView2.text = tailsText
+        }
+
     }
 
     private fun rollDice() {
@@ -22,11 +39,20 @@ class MainActivity : AppCompatActivity() {
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
     }
+
+
 }
 
 class Dice(private val numSides: Int) {
     //rolls dice and produces number in range of 1 and number passed in
     fun roll(): Int {
         return (1..numSides).random()
+    }
+}
+
+class Coin() {
+    // flips coin either 1 heads or 0 tail
+    fun flip(): Int {
+        return (0..1).random()
     }
 }
